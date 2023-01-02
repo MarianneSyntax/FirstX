59cf5a1c1f37d64888331f7b33144b488b5ab50f
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-01-02 08:21:04 +0100

    Initial Commit

---
+++ Shared/FirstXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstXApp.swift
+//  Shared
+//
+//  Created by Marianne Leal on 02.01.23.
+//
+
+import SwiftUI
+
+@main
+struct FirstXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
